f1fd19f238c4eb00bcf36144b9cde7c3a22bc843
R Kar Toe Kyaw <arkartoekyaw@gmail.com>
2023-07-12 02:02:47 +0630

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var birthDate = new Date("1996-11-17");
+var today = new Date();
+var age = today.getFullYear() - birthDate.getFullYear();
+
+// Check if the birthday hasn't occurred yet this month
+if (
+  today.getMonth() < birthDate.getMonth() ||
+  (today.getMonth() === birthDate.getMonth() && today.getDate() <= birthDate.getDate())
+) {
+  age--;
+}
+
+// Update the age element with the calculated age
+document.getElementById("age").innerText = age;
